cda2fd4f368fa0b9d1f402004c8bb2598fee46b5
Semeon Getachew <semeongetachew@Semeons-MacBook-Pro.local>
2020-05-19 10:48:34 -0400

    added about summary

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,19 @@ export default function About() {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
-        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
-        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
-        hic aspernatur error blanditiis?
+        Drink Type showcases the best available drinks offered by the Platoon.
+        It also showcases the name, category, info, glass, instrusctions,
+        and ingredients for each type of drink. All drinks are freshly made in house 
+        to esnure that our customers are served the best. 
+
+        Our waiters and waitresses are fully knowledgeable about the contents of each 
+        freshly made drink, so if you're interested in asking about the ingredients 
+        please feel free to ask! 
+
+        </p>
+        <p>
+        Our Drinks Pack a Punch So Please Drink Responsibly!
+
       </p>
     </section>
   );
